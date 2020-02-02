6278f000f8f1ee5b6a4412fdf536230a620b0f12
Jennifer Pfeffer <jennifer.pfeffer127@gmail.com>
2020-02-02 16:40:02 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.collect do |season, holiday| 
       holiday.collect do |holiday, supplies| 
-        holiday if holiday.include?("BBQ") 
+        holiday if supplies.include?("BBQ") 
         end 
       end 
     end 
